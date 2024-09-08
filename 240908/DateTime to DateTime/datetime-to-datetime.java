b4d8e475502cddf456691dab5b2472d4740cbe35
Sunguk Yang (Kelly) <kelly6bf@gmail.com>
2024-09-08 18:10:25 +0900

    Title: DateTime to DateTime, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240908/DateTime to DateTime/datetime-to-datetime.java
+++ 240908/DateTime to DateTime/datetime-to-datetime.java
@@ -6,27 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int day = sc.nextInt();
-        int hour = sc.nextInt();
-        int min = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        int totalDays = calculateTotalDays(11, day) - calculateTotalDays(11, 11);
-        int totalMin = calculateTotalMin(hour, min) -  calculateTotalMin(11, 11);
-        int result = (totalDays * 24 * 60) + totalMin;
-
+        int result = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
         System.out.println((result >= 0) ? result : -1);
     }
 
-    private static int calculateTotalDays(int mon, int day) {
-        int result = 0;
-        for (int i = 1; i < mon; i++) {
-            result += days[i - 1];
-        }
-
-        return result + day;
-    }
-
-    private static int calculateTotalMin(int hour, int min) {
-        return (hour * 60) + min;
-    }
 }
