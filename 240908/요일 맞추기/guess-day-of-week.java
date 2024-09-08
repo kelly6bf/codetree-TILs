f9e951b9929e558d11f8914ba25d130c353e2112
Sunguk Yang (Kelly) <kelly6bf@gmail.com>
2024-09-08 19:03:50 +0900

    Title: 요일 맞추기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240908/요일 맞추기/guess-day-of-week.java
+++ 240908/요일 맞추기/guess-day-of-week.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
     public static int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    public static String[] weeks = new String[]{"Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"};
+    public static String[] weeks = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,14 +12,20 @@ public class Main {
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
 
-        String result = weeks[(calTotalDays(m2, d2) % 7) + 1];
-        System.out.println(result);
+        int diffDays = (calTotalDays(m2, d2)) - (calTotalDays(m1, d1));
+
+        String result = weeks[(calTotalDays(m2, d2) % 8)];
+        while (diffDays < 0) {
+            diffDays += 7;
+        }
+
+        System.out.println(weeks[diffDays % 7]);
     }
 
     private static int calTotalDays(int m, int d) {
         int result = 0;
-        for (int i = 0; i < m; i++) {
-            result += days[i];
+        for (int i = 1; i < m; i++) {
+            result += days[i - 1];
         }
 
         return result + d;
