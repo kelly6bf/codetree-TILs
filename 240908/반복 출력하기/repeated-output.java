fbbb19c92d83a867ae7cbc687b2dc707f664bbb1
Sunguk Yang (Kelly) <kelly6bf@gmail.com>
2024-09-08 17:11:09 +0900

    Title: 반복 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240908/반복 출력하기/repeated-output.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            printStr();
+        }
+    }
+
+    private static void printStr() {
+        System.out.println("12345^&*()_");
+    }
+}
