a7313863a77f27b49123f68b8f6044ef50d8b989
Sunguk Yang (Kelly) <kelly6bf@gmail.com>
2024-09-09 22:06:41 +0900

    Title: 왔다 갔던 구역 2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240909/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int lineLength = (n * 20) + 1;
+        int[] line = new int[lineLength];
+        int current = lineLength / 2;
+
+        for (int i = 0; i < n; i++) {
+            int distance = sc.nextInt();
+            char command = sc.next().charAt(0);
+            
+            int direction = (command == 'L') ? -1 : 1;
+            for (int j = 0; j < distance; j++) {
+                line[current] += 1;
+                current += direction;
+            }
+            current += direction;
+        }
+
+        int result = 0;
+        for (int i = 0; i < line.length; i++) {
+            if (line[i] >= 2)
+                result++;
+        }
+
+        System.out.println(result);
+    }
+}
