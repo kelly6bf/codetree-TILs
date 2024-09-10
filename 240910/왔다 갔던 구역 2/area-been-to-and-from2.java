5045aade5897474fe421cfdd7a678b97165db478
Sunguk Yang (Kelly) <kelly6bf@gmail.com>
2024-09-10 15:07:58 +0900

    Title: 왔다 갔던 구역 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240910/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240910/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -4,36 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int lineLength = (n * 20) + 1;
-        int[] rightFlow = new int[lineLength];
-        int[] leftFlow = new int[lineLength];
-        int current = lineLength / 2;
+        int[] startPoints = new int[100];
+        int[] endPoints = new int[100];
+        int[] checked = new int[2000 + 1];
+        int offset = 1000;
+        int current = 0;
 
+        int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             int distance = sc.nextInt();
-            char command = sc.next().charAt(0);
-            
-            int direction = (command == 'L') ? -1 : 1;
-            for (int j = 0; j < distance; j++) {
-                if (direction == 'L') {
-                    leftFlow[current] += 1;
-                } else {
-                    rightFlow[current] += 1;
-                }
-                current += direction;
+            char direction = sc.next().charAt(0);
+
+            if (direction == 'L') {
+                startPoints[i] = current - distance;
+                endPoints[i] = current;
+                current -= distance;
+            } else {
+                startPoints[i] = current;
+                endPoints[i] = current + distance;
+                current += distance;
+            }
+
+            startPoints[i] += offset;
+            endPoints[i] += offset;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = startPoints[i]; j < endPoints[i]; j++) {
+                checked[j]++;
             }
         }
 
         int result = 0;
-        for (int i = 0; i < lineLength; i++) {
-            if (leftFlow[i] >= 2) {
+        for (int i = 0; i <= 2000; i++) {
+            if (checked[i] >= 2)
                 result++;
-            }
-
-            if (rightFlow[i] >= 2) {
-                result++;
-            }
         }
 
         System.out.println(result);
